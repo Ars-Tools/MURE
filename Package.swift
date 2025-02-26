a664e6213d73949d4c25d06659ca3e8eb8321b71
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-02-26 15:37:17 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -26,9 +26,9 @@ let package = Package(
 		.executableTarget(
 			name: "Snippets",
 			dependencies: [
-//				.targetItem(name: "Artworks", condition: .none),
-//				.targetItem(name: "CIArtworks", condition: .none),
-				.productItem(name: "Graphics", package: .none, moduleAliases: .none, condition: .none)
+				.targetItem(name: "Artworks", condition: .none),
+				.targetItem(name: "CIArtworks", condition: .none),
+//				.productItem(name: "Graphics", package: .none, moduleAliases: .none, condition: .none)
 			],
 			path: "Snippets/Sources"
 		),
